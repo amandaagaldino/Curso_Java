4a8a83edfcb67bffd43c7ac0d3a8f7bb9c175649
amandaagaldino <amandagaldinox@gmail.com>
2024-12-15 20:48:46 -0300

    Initial commit

---
+++ src/Estudos.java
@@ -0,0 +1,6 @@
+public class Estudos {
+    public static void main (String[] args){
+        System.out.println("Aula 01 Primeiro programa em java - Alura One");
+        System.out.println("Professores: Paulo Silveira e Jaqueline Oliveira");
+    }
+}
---
+++ src/Perfil.java
@@ -0,0 +1,8 @@
+public class Perfil {
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!");
+        String nome = "Amanda";
+        System.out.println("Olá " + nome);
+        System.out.println("Tudo bom?");
+    }
+}
